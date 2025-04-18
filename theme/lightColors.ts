5fbf7b8b72abfad22f08ed3628dc698bc9b6e801
usaif1 <saif.ullah@fuelbuddy.in>
2025-04-18 19:21:37 +0530

    added theme files

---
+++ theme/lightColors.ts
@@ -0,0 +1,109 @@
+export enum ColorPrimaryLight {
+  'primary50' = '#F5F4FB',
+  'primary100' = '#ECE9F8',
+  'primary200' = '#DCD7F3',
+  'primary300' = '#C2B8E8',
+  'primary400' = '#A492DD',
+  'primary500' = '#8569CC',
+  'primary600' = '#6D48B8',
+  'primary700' = '#603DA2',
+  'primary800' = '#4F3083',
+  'primary900' = '#402967',
+  'primary950' = '#271A40',
+}
+
+export enum ColorSecondaryLight {
+  'neutral50' = '#F5F5F7',
+  'neutral100' = '#E9E9EC',
+  'neutral200' = '#D2D2D6',
+  'neutral300' = '#AAABB3',
+  'neutral400' = '#80818B',
+  'neutral500' = '#62636E',
+  'neutral600' = '#4D4D57',
+  'neutral700' = '#3D3D44',
+  'neutral800' = '#333338',
+  'neutral900' = '#2C2C30',
+  'neutral950' = '#111113',
+}
+
+export enum ColorAccentLight {
+  'accent50' = '#FFFDF6',
+  'accent100' = '#FEFAEC',
+  'accent200' = '#FCEFC7',
+  'accent300' = '#F9DF90',
+  'accent400' = '#F7D46B',
+  'accent500' = '#F5C946',
+  'accent600' = '#C8A438',
+  'accent700' = '#9A7E2A',
+  'accent800' = '#6D591D',
+  'accent900' = '#564616',
+  'accent950' = '#292108',
+}
+
+export enum ColorInfoLight {
+  'info50' = '#F5FBFF',
+  'info100' = '#F0F9FF',
+  'info200' = '#E0F2FE',
+  'info300' = '#B9E6FE',
+  'info400' = '#7CD4FD',
+  'info500' = '#36BFFA',
+  'info600' = '#0BA5EC',
+  'info700' = '#0086C9',
+  'info800' = '#026AA2',
+  'info900' = '#065986',
+  'info950' = '#0B4A6F',
+}
+
+export enum ColorWarningLight {
+  'warning50' = '#FFFCF5',
+  'warning100' = '#FFFAEB',
+  'warning200' = '#FEF0C7',
+  'warning300' = '#FEDF89',
+  'warning400' = '#FEC84B',
+  'warning500' = '#FDB022',
+  'warning600' = '#F79009',
+  'warning700' = '#DC6803',
+  'warning800' = '#B54708',
+  'warning900' = '#93370D',
+  'warning950' = '#7A2E0E',
+}
+
+export enum ColorErrorLight {
+  'error50' = '#FFFBFA',
+  'error100' = '#FEF3F2',
+  'error200' = '#FEE4E2',
+  'error300' = '#FECDCA',
+  'error400' = '#FDA29B',
+  'error500' = '#F97066',
+  'error600' = '#F04438',
+  'error700' = '#D92D20',
+  'error800' = '#B42318',
+  'error900' = '#912018',
+  'error950' = '#7A271A',
+}
+
+export enum ColorSuccessLight {
+  'success50' = '#ECFAF4',
+  'success100' = '#D2F1DF',
+  'success200' = '#ABE0C7',
+  'success300' = '#79C8A5',
+  'success400' = '#409773',
+  'success500' = '#308A69',
+  'success600' = '#226A50',
+  'success700' = '#1C5240',
+  'success800' = '#173F32',
+  'success900' = '#133229',
+  'success950' = '#0C1B18',
+}
+
+const lightColors = {
+  primary: ColorPrimaryLight,
+  secondary: ColorSecondaryLight,
+  accent: ColorAccentLight,
+  info: ColorInfoLight,
+  warning: ColorWarningLight,
+  error: ColorErrorLight,
+  success: ColorSuccessLight,
+};
+
+export default lightColors;
---
+++ theme/typography.ts
@@ -0,0 +1,85 @@
+const typography = {
+  h1: {
+    fontSize: 42,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  h2: {
+    fontSize: 35,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  h3: {
+    fontSize: 29,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  h4: {
+    fontSize: 24,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  h5: {
+    fontSize: 20,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  title1: {
+    fontSize: 17,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  title2_body1: {
+    fontSize: 14,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  body2: {
+    fontSize: 12,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+  caption: {
+    fontSize: 10,
+    lineHeight: '120%',
+    fontWeight: {
+      regular: 400,
+      medium: 500,
+      bold: 700,
+    },
+  },
+};
+
+export default typography;
