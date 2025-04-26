3e884de8c57929f80233367ea32a74da5ecfc457
Sahil Ahmed <78998671+idesofmarch00@users.noreply.github.com>
2025-04-26 22:30:37 +0530

    eventslist fixes opt

--- modules/calendar/components/EventsList/EventsList.tsx
+++ modules/calendar/components/EventsList/EventsList.tsx
@@ -4,7 +4,7 @@ import {View, StyleSheet, FlatList} from 'react-native';
 import {Body1Title2Bold, Body2Medium, Title3Bold} from '@/components';
 import {useThemeStore} from '@/globalStore';
 import { DotIcon } from '@/assets/calendar';
-import { ColorWarning } from '@/theme/lightColors';
+import { scale, verticalScale } from '@/theme/responsive';
 
 interface IslamicEvent {
   id: string;
@@ -58,24 +58,23 @@ const EventsList: React.FC<EventsListProps> = () => {
       justifyContent: 'space-between',
     },
     list: {
-      paddingHorizontal: 8,
+      paddingHorizontal: scale(8),
     },
     eventItem: {
       flexDirection: 'row',
-      height: 70,
+      height: verticalScale(70),
       alignItems: 'center',
-      gap: 20, 
+      gap: scale(20), 
       borderBottomWidth: 1,
-      borderBottomColor: '#F5F5F5',
-      padding: 16, 
+      borderBottomColor: colors.primary.primary100,
+      padding: scale(16), 
     },
     dateContainer: {
-      width: 38,
-      height: 38,
-      borderRadius: 8,
+      width: scale(38),
+      height: scale(38),
+      borderRadius: scale(8),
       alignItems: 'center',
       justifyContent: 'center',
-      color: colors.primary.primary500,
       backgroundColor: colors.primary.primary50,
     },
     eventContent: {
@@ -84,9 +83,9 @@ const EventsList: React.FC<EventsListProps> = () => {
     daysLeft: {
       alignItems: 'center',
       justifyContent: 'center',
-      paddingLeft: 8,
+      paddingLeft: scale(8),
       flexDirection: 'row',
-      gap: 4,
+      gap: scale(4),
     },
   });
   
@@ -97,25 +96,23 @@ const EventsList: React.FC<EventsListProps> = () => {
       renderItem={({item}) => (
         <View style={styles.eventItem}>
           <View style={styles.dateContainer}>
-            <Body1Title2Bold style={{fontSize: 14}} color="primary">{item.date.split(' ')[0]}</Body1Title2Bold>
-            <Body2Medium style={{fontSize: 10}} color="primary">{item.date.split(' ')[1]}</Body2Medium>
+            <Body1Title2Bold style={{fontSize: scale(14)}} color="primary">{item.date.split(' ')[0]}</Body1Title2Bold>
+            <Body2Medium style={{fontSize: scale(10)}} color="primary">{item.date.split(' ')[1]}</Body2Medium>
           </View>
           
           <View style={styles.eventContent}>
             <View style={styles.titleContainer}><Body1Title2Bold>{item.title}</Body1Title2Bold> {item.daysLeft !== undefined && (
             <View style={styles.daysLeft}>
-              <DotIcon width={10} height={10} />
+              <DotIcon width={scale(10)} height={scale(10)} />
               {item.isToday ? (
-                <Body2Medium style={{fontSize: 12}} color="warning">Today</Body2Medium>
+                <Body2Medium style={{fontSize: scale(12)}} color="warning">Today</Body2Medium>
               ) : (
-                <Body2Medium style={{fontSize: 12}} color="warning">{item.daysLeft} days left</Body2Medium>
+                <Body2Medium style={{fontSize: scale(12)}} color="warning">{item.daysLeft} days left</Body2Medium>
               )}
             </View>
           )}</View>
-            <Body2Medium style={{fontSize: 12}} color="sub-heading">{item.islamicDate}</Body2Medium>
+            <Body2Medium style={{fontSize: scale(12)}} color="sub-heading">{item.islamicDate}</Body2Medium>
           </View>
-          
-          
         </View>
       )}
       contentContainerStyle={styles.list}
